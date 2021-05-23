a2997e56afa8e3d34f3c47ea4f027d4c4e4b84c9
zeintry <alanpo2000@hotmail.com>
2021-05-23 02:46:30 -0700

    Setup network

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import network
+import mnist_loader
+
+def main():
+    training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+    net = network.Network([784, 30, 10])
+
+    net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+    pass
+
+main()
